02a7c1d66319420d01e16030cb2704e053dadddd
Don Mullen <donmullen@gmail.com>
2013-11-22 15:50:12 -0500

    add god-mode and switch to cider

--- init.el
+++ init.el
@@ -18,4 +18,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-link ((t (:inherit link :underline t :weight semi-bold)))))
